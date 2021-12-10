1af53d44fa6d20daa8b9bd14c222131fd78ae911
Evgenii Litvinov <elitvino@cougarnet.uh.edu>
2021-12-09 23:54:16 -0600

    Update linkedlists.py

--- linkedlists.py
+++ linkedlists.py
@@ -11,7 +11,7 @@ list1 = SLinkedList()
 list1.headval = Node("Mon")
 e2 = Node("Tue")
 e3 = Node("Wed")
-# Link first Node to second node
+# Link first Node to second nodee
 list1.headval.nextval = e2
 
 # Link second Node to third node
