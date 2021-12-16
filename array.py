91ae1b3010f0d004eb299ac5e62bffa8fd46c206
Evgenii Litvinov <elitvino@cougarnet.uh.edu>
2021-12-16 16:16:10 -0600

    Update array.py

--- array.py
+++ array.py
@@ -5,3 +5,14 @@ class Solution:
             nums[i] = nums[i] + nums[i-1]
             
         return nums
+
+
+
+class Solution:
+    def buildArray(self, nums: List[int]) -> List[int]:
+        ans = []
+        for i in range(len(nums)):
+            ans.append(nums[nums[i]])
+        
+        return (ans)
+        
