460621f34536b1a9cfc06317c3b1c16b6cf897ac
Evgenii Litvinov <elitvino@cougarnet.uh.edu>
2021-12-19 14:21:20 -0600

    Create arraypractice.py

---
+++ arraypractice.py
@@ -0,0 +1,19 @@
+def removeElement(nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        length = 0 
+
+        for i in range (len(nums)):
+            if nums[i] != val:
+                length = length +1
+            return length
+              
+    
+                
+        
+        
+
+removeElement([3,2,2,3], 3)
