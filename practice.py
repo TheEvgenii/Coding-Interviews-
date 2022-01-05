ec50b48fb7d413c1787a67596cd71d9b1a18961f
Evgenii Litvinov <elitvino@cougarnet.uh.edu>
2022-01-04 23:59:06 -0600

    e

--- practice.py
+++ practice.py
@@ -1,6 +1,6 @@
-arr = [0,1,2,3,4]
-for i in range (len(arr)-1):
-    if arr[i] == 0:
-        arr[-1] == arr[i]
+hashTab = {}
 
-print(arr)
+for i in range (5):
+    hashTab[i]=i+2
+print(hashTab[0])
+print(hashTab)
