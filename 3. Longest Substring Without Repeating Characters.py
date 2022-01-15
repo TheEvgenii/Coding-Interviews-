4f6d2805556454698ae3c0f631f797498a07e0cd
Evgenii Litvinov <elitvino@cougarnet.uh.edu>
2022-01-14 23:19:26 -0600

    1

---
+++ 3. Longest Substring Without Repeating Characters.py
@@ -0,0 +1,12 @@
+s = 'absad'
+
+fstring = ''
+sstring = ''
+b = ''
+        
+for i in range (len(s)):
+    if s[i] not in fstring:
+        fstring += s[i]
+    elif s[i] not 
+                
+print(fstring)
