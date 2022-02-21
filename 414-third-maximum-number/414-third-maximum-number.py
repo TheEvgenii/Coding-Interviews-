4645a6226c1987aab25816a6203f4755145f17e1
Evgenii (Geno) Litvinov <76677815+TheEvgenii@users.noreply.github.com>
2022-02-20 18:23:26 -0600

    Time: 52 ms (88.66%), Space: 15.4 MB (47.96%) - LeetHub

--- 414-third-maximum-number/414-third-maximum-number.py
+++ 414-third-maximum-number/414-third-maximum-number.py
@@ -1,12 +1,20 @@
 class Solution:
     def thirdMax(self, nums: List[int]) -> int:
-        newarr = []
-        maxnum = 0
-        for i in nums:
-            if i not in newarr:
-                newarr.append(i)
-        newarr.sort(reverse=True)
-        if (len(newarr)) < 3:
-            return max(nums)
-        return newarr[2]
+
+        # Make a Set with the input.
+        nums = set(nums)
+
+        # Find the maximum.
+        maximum = max(nums)
+
+        # Check whether or not this is a case where
+        # we need to return the *maximum*.
+        if len(nums) < 3:
+            return maximum
+
+        # Otherwise, continue on to finding the third maximum.
+        nums.remove(maximum)
+        second_maximum = max(nums)
+        nums.remove(second_maximum)
+        return max(nums)
         
