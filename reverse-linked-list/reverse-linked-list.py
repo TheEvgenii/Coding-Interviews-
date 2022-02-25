cc591e72d824e5725d86ccf71a7f7e36b33ff645
Evgenii (Geno) Litvinov <76677815+TheEvgenii@users.noreply.github.com>
2022-02-24 21:02:05 -0600

    Time: 20 ms, Memory: 15.3 MB - LeetHub

---
+++ reverse-linked-list/reverse-linked-list.py
@@ -0,0 +1,26 @@
+
+class Solution():
+    def reverseList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """  
+        prev = None 
+        curr = head
+        
+        while curr:
+            nxt = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nxt
+        return prev
+            
+            
+        
+        
+        
+        
+        
+       
+            
+            
